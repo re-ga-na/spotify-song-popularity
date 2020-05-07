f93f7eb18f7ab35c104a9f95eedf5e3c09599dbc
iris28kurti <iris28kurti@gmail.com>
2020-05-07 11:51:06 -0400

    ik

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,21 @@
 // from data.js
-var tableData = data;
+var dataFeatures = data;
 
-// YOUR CODE HERE!
+// Start with a blank array
+var features = ['0', 'Artist', 'Track', 'Unnamed: 0', 'acousticness', 'danceability',
+'duration_ms', 'energy', 'followers', 'instrumentalness', 'key',
+'liveness', 'loudness', 'mode', 'popularity', 'speechiness', 'tempo',
+'time_signature', 'valence'] ;
+
+// Display the array in console
+console.log("An array of features:");
+console.log(features);
+
+// Use indexing to access an array item
+console.log("Use indexing to access an array item:");
+var artistName = Artist;
+var searchStr = Track;
+console.log(artistName);
+console.log(searchStr);
+
+
